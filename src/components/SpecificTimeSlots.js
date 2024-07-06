87f00b32bc92cb741b9b5a5c6af9db836604ec26
andela-tfowotade <temitope.fowotade@andela.com>
2024-07-07 00:52:41 +0100

    wip

--- src/components/SpecificTimeSlots.js
+++ src/components/SpecificTimeSlots.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button } from "flowbite-react";
+import Calendar from 'react-calendar'
 
 function SpecificTimeSlots() {
   const [showCalendar, setShowCalendar] = useState(false)
@@ -12,7 +13,7 @@ function SpecificTimeSlots() {
       </div>
 
       <div>
-        <div className="">
+        <div className="pt-4">
           <Button
             onClick={() => setShowCalendar(true)}
             outline
@@ -22,8 +23,63 @@ function SpecificTimeSlots() {
           </Button>
         </div>
       </div>
+
+      { showCalendar && <DisplayCalendar />}
     </div>
   )
 }
+
+
+function DisplayCalendar() {
+  const today = new Date();
+  const [date, setDate] = useState(today);
+  const [timeSlots, setTimeSlots] = useState([]);
+
+  const getUpcomingAvailableDates = () => {
+    // display upcoming date-specific hours when calendar is submitted
+  }
+
+  const handleChange = (date) => {
+    setDate(date);
+    // set available slots for date
+  }
+
+  const handleSubmit = () => {
+    // submit booking for student
+  }
+
+  const onTimeSelect = (time) => {
+    console.log(time);
+    // add animation to show confirm button
+  }
+
+  return (
+    <div>
+      <div className="">
+        <div>
+          <h1 className='mb-8'>Select the date(s) you want to assign specific hours</h1>
+        </div>
+        <div className="flex">
+          <div className='mr-8'>
+            <div>
+              <
+                Calendar
+                value={date}
+                onChange={handleChange}
+                view={"month"}
+                minDate={today}
+                prev2Label={null}
+                next2Label={null}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+
 
 export default SpecificTimeSlots;
